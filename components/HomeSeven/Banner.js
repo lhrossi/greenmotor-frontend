2a07cc40aeb187c2fa7dffc89e4308201fe61c47
Derickson Loss <dericksonlossit@gmail.com>
2022-03-20 17:35:36 -0300

    changes main banner

--- components/HomeSeven/Banner.js
+++ components/HomeSeven/Banner.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
-import dynamic from 'next/dynamic';
-const ModalVideo = dynamic(() => import('react-modal-video'), {
-    ssr: false
-});
 
 class Banner extends Component {
     state = {
@@ -11,20 +7,12 @@ class Banner extends Component {
     };
 
     openModal = () => {
-        this.setState({isOpen: true})
+        this.setState({ isOpen: true })
     };
 
     render() {
         return (
             <>
-                {/* If you want change the video need to update below videoID */}
-                <ModalVideo 
-                    channel='youtube' 
-                    isOpen={this.state.isOpen} 
-                    videoId='szuchBiLrEM' 
-                    onClose={() => this.setState({isOpen: false})} 
-                />
-            
                 <div className="saas-banner-two">
                     <div className="d-table">
                         <div className="d-table-cell">
@@ -32,72 +20,28 @@ class Banner extends Component {
                                 <div className="row align-items-center">
                                     <div className="col-lg-6">
                                         <div className="banner-content">
-                                            <h1>Manage Your Digital Marketing in One Place Taiker</h1>
-                                            <p>We work hand-in-hand with industry-leading brands to help redefine the possibilities and potential of digital engagements.</p>
-                                            
-                                            <div className="banner-btn">
-                                                <Link href="/contact">
-                                                    <a className="default-btn mr-4">
-                                                        Get Started <span></span>
-                                                    </a>
-                                                </Link>
+                                            <h1 className='main-banner-title-color'>Nosso propósito é menos desperdício de alimentos e mais lucro aos supermercados</h1>
+                                            <p className='main-banner-title-color'>Utilizando os dados que seu mercado já tem, utilizamos nossa Inteligência Artificial para <b>otimizar seu estoque</b>, <b>diminuir o desperdício</b> de alimentos e fazer o seu cliente ter um <b>produto mais fresco</b>, enquanto <b>aumentamos o retorno do seu investimento.</b></p>
 
-                                                <Link href="/index7">
+                                            <div className="banner-btn">
+                                                <Link href="/#">
                                                     <a className="default-btn-two">
-                                                        Try It Now <span></span>
+                                                        Teste Grátis <span></span>
                                                     </a>
                                                 </Link>
                                             </div>
                                         </div>
                                     </div>
-
                                     <div className="col-lg-6">
                                         <div className="saas-banner-image animate-tb">
                                             <img src="/images/saas-banner/dashboard.png" alt="image" />
-
-                                            <div className="solution-video">
-                                                <div
-                                                    onClick={e => {e.preventDefault(); this.openModal()}}
-                                                    className="video-btn popup-youtube"
-                                                > 
-                                                    <i className="flaticon-play-button"></i>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
-                    {/* Shape Images */}
-                    <div className="shape-img2">
-                        <img src="/images/shape/shape2.svg" alt="image" />
-                    </div>
-                    <div className="shape-img3">
-                        <img src="/images/shape/shape3.svg" alt="image" />
-                    </div>
-                    <div className="shape-img4">
-                        <img src="/images/shape/shape4.png" alt="image" />
-                    </div>
-                    <div className="shape-img5">
-                        <img src="/images/shape/shape5.png" alt="image" />
-                    </div>
-                    <div className="shape-img6">
-                        <img src="/images/shape/shape6.png" alt="image" />
-                    </div>
-                    <div className="shape-img7">
-                        <img src="/images/shape/shape7.png" alt="image" />
-                    </div>
-                    <div className="shape-img8">
-                        <img src="/images/shape/shape8.png" alt="image" />
-                    </div>
-                    <div className="shape-img9">
-                        <img src="/images/shape/shape9.png" alt="image" />
-                    </div>
-                    <div className="shape-img10">
-                        <img src="/images/shape/shape10.png" alt="image" />
-                    </div>        
+
                 </div>
             </>
         );
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Layout/Navbar';
-import BannerSlider from '../components/HomeEight/BannerSlider';
 import FeaturedSolutions from '../components/HomeEight/FeaturedSolutions';
 import FunFacts from '../components/Common/FunFacts';
 import FreeTrial from '../components/HomeEight/FreeTrial';
 import Footer from '../components/Layout/Footer';
 import Clients from '../components/HomeEight/Clients';
+import Banner from '../components/HomeSeven/Banner';
+
 
 class Index extends Component {
     render() {
         return (
             <>
                 <Navbar />
-                <BannerSlider />
+                <Banner />
                 <FeaturedSolutions />
                 <FunFacts />
-                <Clients />
+                {/* <Clients /> */}
                 <FreeTrial />
                 <Footer/> 
             </>
