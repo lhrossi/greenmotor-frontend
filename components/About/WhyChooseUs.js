0dc93a5cb97a62bdb188050b0233c15e0b7b70d1
Derickson Loss <dericksonlossit@gmail.com>
2022-03-22 01:30:10 -0300

    alter about us component

--- components/About/WhyChooseUs.js
+++ components/About/WhyChooseUs.js
@@ -1,33 +1,33 @@
+import Image from 'next/image';
 import React, { Component } from 'react';
+import BlackLogoImage from '../../images/BlackLogoTransparentBackground.png';
+
 
 class WhyChooseUs extends Component {
     render() {
         return (
             <>
-                <section className="why-choose-area ptb-100">
+                <section className="why-choose-area ptb-100 bg-gray">
                     <div className="container">
                         <div className="row align-items-center">
                             <div className="col-lg-6 col-md-12">
+                                <br />
+                                <br />
+                                <br />
                                 <div className="why-choose-content">
-                                    <span className="sub-title">Why Choose Us</span>
-                                    <h2>The Key To Your Motivation And Success</h2>
-                                    <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication.</p>
-
-                                    <div className="features-text">
-                                        <h4><i className="flaticon-tick"></i> Core Development</h4>
-                                        <p>No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
-                                    </div>
-
-                                    <div className="features-text">
-                                        <h4><i className="flaticon-tick"></i> Define Your Choices</h4>
-                                        <p>No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
-                                    </div>
+                                    <h2>A Plataforma digital que elimina o desperdício de alimentos perecíveis de forma inteligente.</h2>
+                                    <br />
+                                    <br />
+                                    <p>A Greenmotor é uma plataforma de previsão automatizada de IA altamente precisa, trazendo resultados assertivos de  previsão de vendas, estoque ideal, proposta de compras e pricing para o segmento dos supermercados.</p>
                                 </div>
                             </div>
 
                             <div className="col-lg-6 col-md-12">
-                                <div className="why-choose-image">
-                                    <img src="/images/why-choose-img1.png" alt="image" />
+                                <br />
+                                <br />
+                                <br />
+                                <div className="why-choose-image rotate">
+                                    <Image src={BlackLogoImage} alt="Logo greenmotor girando" />
                                 </div>
                             </div>
                         </div>
