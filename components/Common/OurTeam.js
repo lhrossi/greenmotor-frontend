87749933132812e1d70d14a60b83cd2e58177455
Derickson Loss <dericksonlossit@gmail.com>
2022-03-22 01:30:16 -0300

    alters team component

--- components/Common/OurTeam.js
+++ components/Common/OurTeam.js
@@ -5,112 +5,36 @@ class OurTeam extends Component {
     render() {
         return (
             <>
-                <section className="team-area ptb-100 pb-70 pt-0">
+                <section className="team-area ptb-100 pb-70 pt-0 bg-black">
                     <div className="container">
-                        <div className="section-title">
-                            <span className="sub-title">Our Team</span>
-                            <h2>Meet Our Awesome Team Member</h2>
+                        <div className="section-title sobre-nos center-everything">
+                            <br />
+                            <br />
+                            <br />
+                            <h2 className='text-color-white'>Sobre os Founders</h2>
                         </div>
-
-                        <div className="row justify-content-center">
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-team-box">
-                                    <div className="image">
-                                        <img src="/images/team-image/team1.jpg" alt="team" />
-                                        
-                                        <div className="social">
-                                            <a href="https://www.facebook.com/" target="_blank">
-                                                <i className="fab fa-facebook-f"></i>
-                                            </a>
-                                            <a href="https://twitter.com/" target="_blank">
-                                                <i className="fab fa-twitter"></i>
-                                            </a>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <i className="fab fa-instagram"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    
-                                    <div className="content">
-                                        <h3>John Smith</h3>
-                                        <span>Web Developer</span>
-                                    </div>
+                        <div className='flex-about-us'>
+                            <div className="col-lg-6 col-md-12">
+                                <div className='sobre-nos'>
+                                    <br />
+                                    <h1 className='text-color-white'>EDUARDO KAUTZ</h1>
+                                    <br />
+                                    <h2 className='text-color-white'>Founder & CEO</h2>
+                                    <br />
+                                    <p className='text-color-white'>Fundador e sócio da Play Delivery, plataforma digital de intermediação em entregas que está em mais de 130 cidades no Brasil, com mais de 100 mil entregadores cadastrados e mais de 7 milhões de entregas realizadas.</p>
                                 </div>
                             </div>
-
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-team-box">
-                                    <div className="image">
-                                        <img src="/images/team-image/team2.jpg" alt="team" />
-                                        
-                                        <div className="social">
-                                            <a href="https://www.facebook.com/" target="_blank">
-                                                <i className="fab fa-facebook-f"></i>
-                                            </a>
-                                            <a href="https://twitter.com/" target="_blank">
-                                                <i className="fab fa-twitter"></i>
-                                            </a>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <i className="fab fa-instagram"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <h3>Lucy Eva</h3>
-                                        <span>Manager</span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-team-box">
-                                    <div className="image">
-                                        <img src="/images/team-image/team3.jpg" alt="team" />
-
-                                        <div className="social">
-                                            <a href="https://www.facebook.com/" target="_blank">
-                                                <i className="fab fa-facebook-f"></i>
-                                            </a>
-                                            <a href="https://twitter.com/" target="_blank">
-                                                <i className="fab fa-twitter"></i>
-                                            </a>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <i className="fab fa-instagram"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <h3>Steven Smith</h3>
-                                        <span>Web Developer</span>
-                                    </div>
+                            <div className="col-lg-6 col-md-12">
+                                <div className='sobre-nos'>
+                                    <br />
+                                    <h1 className='text-color-white' >RODRIGO VILLA REAL</h1>
+                                    <br />
+                                    <h2 className='text-color-white'>Co-founder & CFO</h2>
+                                    <br />
+                                    <p className='text-color-white'>Oito anos de experiência no mercado financeiro, com passagem pelo Banco Renner, Azimut Wealth Management, gestora italiana, e Crescendo Group, boutique suíça. Especialista em Wealth Management. Relaciona-se e investe no ecossistema de startups há três anos.</p>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    {/* Animation shape image */}
-                    <div className="shape-img2">
-                        <img src="/images/shape/shape2.svg" alt="image" />
-                    </div>
-                    <div className="shape-img3">
-                        <img src="/images/shape/shape3.svg" alt="image" />
-                    </div>
-                    <div className="shape-img4">
-                        <img src="/images/shape/shape4.png" alt="image" />
-                    </div>
-                    <div className="shape-img5">
-                        <img src="/images/shape/shape5.png" alt="image" />
-                    </div>
-                    <div className="shape-img6">
-                        <img src="/images/shape/shape6.png" alt="image" />
-                    </div>
-                    <div className="shape-img9">
-                        <img src="/images/shape/shape9.png" alt="image" />
-                    </div>
-                    <div className="shape-img10">
-                        <img src="/images/shape/shape10.png" alt="image" />
                     </div>
                 </section>
             </>
