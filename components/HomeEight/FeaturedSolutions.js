a57da2d15a73079c2c4dfa1282ef5aa6a61f8d0b
Derickson Loss <dericksonlossit@gmail.com>
2022-03-21 22:29:38 -0300

    alters featured solution styling

--- components/HomeEight/FeaturedSolutions.js
+++ components/HomeEight/FeaturedSolutions.js
@@ -4,23 +4,23 @@ import Link from 'next/link';
 class FeaturedSolutions extends Component {
     render() {
         return (
-            <section className="pt-100 pb-70 bg-black">
+            <section className="pt-100 pb-70 bg-gray">
                 <div className="container">
                     <div className="section-title">
-                        <h2 className='main-banner-title-color'>Nossos diferenciais</h2>
+                        <h2 className='text-color-black'>Nossos diferenciais</h2>
                         {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p> */}
                     </div>
 
                     <div className="row">
                         <div className="col-lg-3 col-sm-6">
-                            <div className="featured-solution-card">
+                            <div className="featured-solution-card bg-black">
                                 <i className='bx bxs-analyse'></i>
-                                <h3>
+                                <h3 className=''>
                                     <Link href="/service-details">
                                         <a>Extração de Dados</a>
                                     </Link>
                                 </h3>
-                                <p>Integramos com os seus sistemas de gestão empresarial e de estoque</p>
+                                <p className='text-color-white'>Integramos com os seus sistemas de gestão empresarial e de estoque</p>
                                 <Link href="/service-details">
                                     <a className="read-more">Saber mais</a>
                                 </Link>
@@ -35,7 +35,7 @@ class FeaturedSolutions extends Component {
                                         <a>Fácil Visualização</a>
                                     </Link>
                                 </h3>
-                                <p>Tenha insights mais rápidos com nossos dashboards personalizados</p>
+                                <p className='text-color-white'>Tenha insights mais rápidos com nossos dashboards personalizados</p>
                                 <Link href="/service-details">
                                     <a className="read-more">Saber mais</a>
                                 </Link>
@@ -50,7 +50,7 @@ class FeaturedSolutions extends Component {
                                         <a>Previsão de Vendas</a>
                                     </Link>
                                 </h3>
-                                <p>Tenha uma maior assertividade sobre quando e quanto comprar </p>
+                                <p className='text-color-white'>Tenha uma maior assertividade sobre quando e quanto comprar </p>
                                 <Link href="/service-details">
                                     <a className="read-more">Saber mais</a>
                                 </Link>
@@ -65,7 +65,7 @@ class FeaturedSolutions extends Component {
                                         <a>Modelos de IA sofisticados</a>
                                     </Link>
                                 </h3>
-                                <p>Aproveite cada informação já disponível nos seus sistemas</p>
+                                <p className='text-color-white'>Aproveite cada informação já disponível nos seus sistemas</p>
                                 <Link href="/service-details">
                                     <a className="read-more">Saber mais</a>
                                 </Link>
