830c0d43c923fbfce18b858ce1e643ba9442b6ee
Derickson Loss <dericksonlossit@gmail.com>
2022-03-21 22:28:17 -0300

    change styling and images of client components

--- components/HomeEight/Clients.js
+++ components/HomeEight/Clients.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import LogoDiaCinza from '../../images/mercadoDiaLogo.png';
+import LogoMagmaxxCinza from '../../images/mercadoMagmaxxLogo.png';
 
 class Clients extends Component {
     render() {
         return (
             <>
-                <section className="partner-area ptb-100 bg-black">
+                <section className="partner-area ptb-100 bg-gray">
                     <div className="container">
                         <div className="section-title">
-                            <h2 className='text-color-white'>Nossos parceiros</h2>
-                            <p className='text-color-white'>Supermercados que já estão utilizando nossa tecnologia para redução de desperdícios.</p>
+                            <h2 className='text-color-black'>Nossos parceiros</h2>
+                            <p className='text-color-black'>Supermercados que já estão utilizando nossa tecnologia para redução de desperdícios.</p>
                         </div>
 
                         <div className="row align-items-center col-lg-12">
-                            <div className="single-partner-item">
-                                <a href="#" target="_blank">
-                                    <img src="/images/mercadoDiaLogo.png" alt="image" />
-                                    <img src="/images/mercadoDiaLogo.png" alt="image" />
-                                </a>
+                            <div className='custom-partner-item'>
+                                <Image src={LogoDiaCinza} alt="Logo supermercado dia" className='' />
                             </div>
-                            <div className="single-partner-item">
-                                <a href="#" target="_blank">
-                                    <img src="/images/mercadoMagmaxxLogo.png" alt="image" />
-                                    <img src="/images/mercadoMagmaxxLogo.png" alt="image" />
-                                </a>
+                            <div className='custom-partner-item'>
+                                <Image src={LogoMagmaxxCinza} alt="Logo supermercado magmaxx" className='' />
                             </div>
                         </div>
                     </div>
