34eebc74ac4a75e1f5393e45a4b370c25ff3c7d0
dericksonlossit <dericksonlossit@gmail.com>
2022-03-23 17:23:40 -0300

    WIP add company images to founder section

--- components/Common/OurTeam.js
+++ components/Common/OurTeam.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import eduImage from '../../images/eduPhoto.png';
 import rodrigoImage from '../../images/rodrigoPhoto.png';
+import azimutImage from '../../images/azimutPhoto.png';
+import playDeliveryImage from '../../images/playDeliveryPhoto.png';
+import crescendoImage from '../../images/crescendoLogo.png';
 
 class OurTeam extends Component {
     render() {
@@ -26,6 +29,7 @@ class OurTeam extends Component {
                                     <h2 className='text-color-white'>Founder & CEO</h2>
                                     <br />
                                     <p className='text-color-white'>Fundador e sócio da Play Delivery, plataforma digital de intermediação em entregas que está em mais de 130 cidades no Brasil, com mais de 100 mil entregadores cadastrados e mais de 7 milhões de entregas realizadas.</p>
+                                    <Image src={playDeliveryImage} alt="Logo da PlayDelivery" />
                                 </div>
                             </div>
                             <div className="col-lg-6 col-md-12">
@@ -37,6 +41,8 @@ class OurTeam extends Component {
                                     <h2 className='text-color-white'>Co-founder & CFO</h2>
                                     <br />
                                     <p className='text-color-white'>Oito anos de experiência no mercado financeiro, com passagem pelo Banco Renner, Azimut Wealth Management, gestora italiana, e Crescendo Group, boutique suíça. Especialista em Wealth Management. Relaciona-se e investe no ecossistema de startups há três anos.</p>
+                                    <Image src={azimutImage} alt="Logo da Azimut" />
+                                    <Image src={crescendoImage} alt="Logo da Crescendo" />
                                 </div>
                             </div>
                         </div>
