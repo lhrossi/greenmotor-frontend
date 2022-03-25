44d1767d854a394cc762ef847abb07a23cc1986d
Derickson Loss <dericksonlossit@gmail.com>
2022-03-24 22:01:45 -0300

    adds clients component back to main page

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ class Index extends Component {
                 <FeaturedSolutions />
                 <FunFacts />
                 <Press />
+                <Clients />
                 <FreeTrial />
                 <Footer/> 
             </>
