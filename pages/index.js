507d754887af18cf162d097ad73ff2d9e6cb44bc
dericksonlossit <dericksonlossit@gmail.com>
2022-04-06 09:58:38 -0300

    return page components

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import Navbar from '../components/Layout/Navbar';
-import FeaturedSolutions from '../components/HomeEight/FeaturedSolutions';
-import FunFacts from '../components/Common/FunFacts';
-import FreeTrial from '../components/HomeEight/FreeTrial';
-import Footer from '../components/Layout/Footer';
-import Clients from '../components/HomeEight/Clients';
-import Banner from '../components/HomeSeven/Banner';
-import Press from '../components/Common/Press';
-import InDevelopment from '../components/Common/InDevelopment';
-
+import React, { Component } from "react";
+import Navbar from "../components/Layout/Navbar";
+import FeaturedSolutions from "../components/HomeEight/FeaturedSolutions";
+import FunFacts from "../components/Common/FunFacts";
+import FreeTrial from "../components/HomeEight/FreeTrial";
+import Footer from "../components/Layout/Footer";
+import Clients from "../components/HomeEight/Clients";
+import Banner from "../components/HomeSeven/Banner";
+import Press from "../components/Common/Press";
+import InDevelopment from "../components/Common/InDevelopment";
 
 class Index extends Component {
-    render() {
-        return (
-            <>
-            <InDevelopment />
-                {/* <Navbar />
-                <Banner />
-                <FeaturedSolutions />
-                <FunFacts />
-                <Press />
-                <Clients />
-                <FreeTrial />
-                <Footer/>  */}
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        {/* <InDevelopment /> */}
+        <Navbar />
+        <Banner />
+        <FeaturedSolutions />
+        <FunFacts />
+        <Press />
+        <Clients />
+        <FreeTrial />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default Index;
