afca530a13483b2b4e000fdabd76f2e6fe0f9c8b
dericksonlossit <dericksonlossit@gmail.com>
2022-07-05 13:02:57 -0300

    changes logo order and removes links

--- components/Common/AcceleratedBySection.js
+++ components/Common/AcceleratedBySection.js
@@ -20,57 +20,42 @@ const AcceleratedBySection = () => {
                     <div className="row">
                         <div className="col-lg-3 col-sm-6">
                             <div className="custom-partner-item">
-                                <a
-                                    href="https://www.navi.com.ai/"
-                                    target="_blank"
-                                >
-                                    <Image
-                                        src={naviPhoto}
-                                        alt="Logo do Hub NAVI"
-                                        className=""
-                                    />
-                                </a>
+                                <Image
+                                    src={oraclePhoto}
+                                    alt="Logo da Oracle for Startups"
+                                    className=""
+                                />
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="custom-partner-item">
+                                <Image
+                                    src={naviPhoto}
+                                    alt="Logo do Hub NAVI"
+                                    className=""
+                                />
                             </div>
                         </div>
 
-                        <div className="col-lg-3 col-sm-6"><div className="custom-partner-item">
-                                <a
-                                    href="https://www.oracle.com/br/startup/"
-                                    target="_blank"
-                                >
-                                    <Image
-                                        src={oraclePhoto}
-                                        alt="Logo da Oracle for Startups"
-                                        className=""
-                                    />
-                                </a>
-                            </div></div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="custom-partner-item">
+                                <Image
+                                    src={banritechPhoto}
+                                    alt="Logo do Banritech"
+                                    className=""
+                                />
+                            </div>
+                        </div>
 
-                        <div className="col-lg-3 col-sm-6"><div className="custom-partner-item">
-                                <a
-                                    href="https://promo.banrisul.com.br/bdg/link/banritech.html"
-                                    target="_blank"
-                                >
-                                    <Image
-                                        src={banritechPhoto}
-                                        alt="Logo do Banritech"
-                                        className=""
-                                    />
-                                </a>
-                            </div></div>
-
-                        <div className="col-lg-3 col-sm-6"><div className="custom-partner-item">
-                                <a
-                                    href="https://www.inovativa.online/"
-                                    target="_blank"
-                                >
-                                    <Image
-                                        src={inovativaPhoto}
-                                        alt="Logo da Inovativa Brasil"
-                                        className=""
-                                    />
-                                </a>
-                            </div></div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="custom-partner-item">
+                                <Image
+                                    src={inovativaPhoto}
+                                    alt="Logo da Inovativa Brasil"
+                                    className=""
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
