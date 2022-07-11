a15937d4e4dfee3ef69ad9dd8b3d933c8d01938e
dericksonlossit <dericksonlossit@gmail.com>
2022-07-11 10:27:51 -0300

    fixes video and amcham logo

--- components/Common/VideoSection.js
+++ components/Common/VideoSection.js
@@ -10,7 +10,7 @@ const VideoSection = () => {
                         height="460"
                         src="https://www.youtube.com/embed/MTxRk1_1RUs"
                         title="YouTube video player"
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen
                     ></iframe>
