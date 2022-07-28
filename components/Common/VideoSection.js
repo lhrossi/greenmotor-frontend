9b35d2d4834c48f63ef081c2ff43d42783c24a54
Derickson Loss <dericksonlossit@gmail.com>
2022-07-28 07:46:10 -0300

    fixes banner button position

--- components/Common/VideoSection.js
+++ components/Common/VideoSection.js
@@ -12,7 +12,7 @@ const VideoSection = () => {
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullscreen
+                        allowFullScreen
                     ></iframe>
                 </div>
             </div>
