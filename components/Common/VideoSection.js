99201e23ed193cb8b31a5838916ffecbd56d133d
dericksonlossit <dericksonlossit@gmail.com>
2022-08-10 16:24:34 -0300

    removes the old video

--- components/Common/VideoSection.js
+++ components/Common/VideoSection.js
@@ -8,15 +8,6 @@ const VideoSection = () => {
                     <iframe
                         width="560"
                         height="315"
-                        src="https://www.youtube.com/embed/MTxRk1_1RUs"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                    <iframe
-                        width="560"
-                        height="315"
                         src="https://www.youtube.com/embed/dLxkUnFmxHo"
                         title="YouTube video player"
                         frameborder="0"
