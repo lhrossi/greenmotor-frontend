bfc93c97ba9f61af2feb6d06c13038c18b5c5ec7
dericksonlossit <dericksonlossit@gmail.com>
2022-08-10 16:19:31 -0300

    changes video fullscreen title

--- components/Common/VideoSection.js
+++ components/Common/VideoSection.js
@@ -3,16 +3,25 @@ import React from "react";
 const VideoSection = () => {
     return (
         <section className="pb-70 bg-gray">
-            <div className="container">
-                <div className="row align-items-center col-lg-12">
+            <div className="container custom-video-section">
+                <div className="row align-items-center col-lg-6 ">
                     <iframe
-                        width="360"
-                        height="460"
+                        width="560"
+                        height="315"
                         src="https://www.youtube.com/embed/MTxRk1_1RUs"
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
+                    ></iframe>
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/dLxkUnFmxHo"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullscreen
                     ></iframe>
                 </div>
             </div>
