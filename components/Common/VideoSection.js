ee1942cdbea113f82c703c091045602c297c58f5
dericksonlossit <dericksonlossit@gmail.com>
2022-08-10 16:22:15 -0300

    Removes the old video

--- components/Common/VideoSection.js
+++ components/Common/VideoSection.js
@@ -4,16 +4,7 @@ const VideoSection = () => {
     return (
         <section className="pb-70 bg-gray">
             <div className="container custom-video-section">
-                <div className="row align-items-center col-lg-6 ">
-                    <iframe
-                        width="560"
-                        height="315"
-                        src="https://www.youtube.com/embed/MTxRk1_1RUs"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
+                <div className="align-items-center col-lg-6 custom-video-section">
                     <iframe
                         width="560"
                         height="315"
