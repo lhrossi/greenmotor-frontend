d94504492c388f5dee4ba206758f7c54a3245ce6
Luiz Henrique Rossi <lh.rossi@cognitivabrasil.com.br>
2022-08-16 18:18:30 -0300

    endpoint para bellitech

--- components/HomeEight/FreeTrial.js
+++ components/HomeEight/FreeTrial.js
@@ -3,7 +3,18 @@ import Image from 'next/image';
 import BlackLogoImage from '../../images/BlackLogoTransparentBackground.png';
 import WhiteLogoImage from '../../images/WhiteLogoTransparentBackground.png';
 
+var phone = '555198668926';
+
 class FreeTrial extends Component {
+
+    componentDidMount(){
+        
+        console.debug(this.phone);
+        if (this.props.rep === 'bellitech') {
+            phone = '555184747319';            
+          }
+    }
+    
     render() {
         return (
             <section className="free-trial-area ptb-100 bg-black">
@@ -17,12 +28,16 @@ class FreeTrial extends Component {
                         <div className="col-lg-6">
                             <div className="subscribe-content subscribe-content-custom">
                                 <h2 className='text-color-white'>Agende uma demonstração gratuita</h2>
-                                <a href='https://api.whatsapp.com/send?phone=555198668926&text=Ol%C3%A1!%20Poderia%20me%20enviar%20mais%20informa%C3%A7%C3%B5es%20sobre%20a%20Greenmotor%3F' target='_blank' className='custom-free-trial-button button-white-background'>Marcar uma demonstração</a>
+                                <a href={'https://api.whatsapp.com/send?phone=' + phone + '&text=Ol%C3%A1!%20Poderia%20me%20enviar%20mais%20informa%C3%A7%C3%B5es%20sobre%20a%20Greenmotor%3F'} target='_blank' className='custom-free-trial-button button-white-background'>Marcar uma demonstração</a>
                                 <span className="help-text text-color-white">Teste nossa Inteligência Artificial por 15 dias gratuitos.</span>
                             </div>
                         </div>
                     </div>
                 </div>
+                
+                    <a href={'https://api.whatsapp.com/send?phone=' + phone + '&text=Ol%C3%A1!%20Poderia%20me%20enviar%20mais%20informa%C3%A7%C3%B5es%20sobre%20a%20Greenmotor%3F'} className="float-wpp" target="_blank">
+                        <i aria-hidden className="fa fa-whatsapp my-float-wpp"></i>
+                    </a>
             </section>
         );
     }
