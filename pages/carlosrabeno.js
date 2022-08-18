9094b8e5bb7b6d3c8d27288fd460a172671bd15e
Luiz Henrique Rossi <lh.rossi@cognitivabrasil.com.br>
2022-08-18 10:45:03 -0300

    incluindo representante carlos

---
+++ pages/carlosrabeno.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Navbar from "../components/Layout/Navbar";
+import FeaturedSolutions from "../components/HomeEight/FeaturedSolutions";
+import FunFacts from "../components/Common/FunFacts";
+import VideoSection from "../components/Common/VideoSection";
+import FreeTrial from "../components/HomeEight/FreeTrial";
+import Footer from "../components/Layout/Footer";
+import Banner from "../components/HomeSeven/Banner";
+import Press from "../components/Common/Press";
+import DashboardSection from "../components/Common/DashboardSection";
+import AcceleratedBySection from "../components/Common/AcceleratedBySection";
+import TecnopucSection from "../components/Common/TecnopucSection";
+import AmchamSection from "../components/Common/AmchamSection";
+
+class Index extends Component {
+  render() {
+    return (
+      <>
+        <Navbar repphone='5551991632731'/>
+        <Banner />
+        <DashboardSection />
+        <Press />
+        <AcceleratedBySection />
+        <TecnopucSection />
+        {/* <AmchamSection /> */}
+        <VideoSection />
+        <FeaturedSolutions />
+        <FunFacts />
+        <FreeTrial repphone='5551991632731'/>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Index;
