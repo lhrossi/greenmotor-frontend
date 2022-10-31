d635cf7fc82f0d1a124456f5e908f30ea647a55d
Luiz Henrique Rossi <lh.rossi@cognitivabrasil.com.br>
2022-10-31 11:41:19 -0300

    alteração da ordem das equipes

--- components/Common/OurTeam.js
+++ components/Common/OurTeam.js
@@ -38,7 +38,7 @@ class OurTeam extends Component {
                                     <br />
                                     <h1 className='text-color-white text-margin-top'>EDU <br /> KAUTZ</h1>
                                     <br />
-                                    <h2 className='text-color-white'>Founder & CEO</h2>
+                                    <h2 className='text-color-white'>Founder &amp; CEO</h2>
                                     <br />
                                     <p className='text-color-white'>Fundador e sócio da Play Delivery, plataforma digital de intermediação em entregas que está em mais de 130 cidades no Brasil, com mais de 100 mil entregadores cadastrados e mais de 7 milhões de entregas realizadas.</p>
                                     <Image src={playDeliveryImage} alt="Logo da PlayDelivery" />
@@ -49,7 +49,7 @@ class OurTeam extends Component {
                                     <Image src={rodrigoImage} alt="Foto do co-fundador Rodrigo Villa" />
                                     <h1 className='text-color-white text-margin-top' >RODRIGO VILLA REAL</h1>
                                     <br />
-                                    <h2 className='text-color-white'>Co-Founder & CFO</h2>
+                                    <h2 className='text-color-white'>Co-Founder &amp; CFO</h2>
                                     <br />
                                     <p className='text-color-white'>Oito anos de experiência no mercado financeiro, com passagem pelo Banco Renner, Azimut Wealth Management e Crescendo Group. Especialista em Wealth Management. Relaciona-se e investe no ecossistema de startups há três anos.</p>
                                     <Image src={azimutImage} alt="Logo da Azimut" />
@@ -69,43 +69,6 @@ class OurTeam extends Component {
                                     <br />
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </section>
-
-                <section className="team-area ptb-100 pb-70 pt-0 bg-black">
-                    <div className="container">
-                        <div className="section-title sobre-nos center-everything">
-                            <br />
-                            <br />
-                            <br />
-                            <h2 className='text-color-white'>Conselho de Tecnologia</h2>
-                        </div>
-                        <div className='flex-about-us'>
-                            <div className="col-lg-6 col-md-12">
-                                <div className='sobre-nos-2-col'>
-                                    <Image width={195} height={195} src={nichoImage} alt="Foto do fundador Eduardo Kautz" className='founder-image-padding' />
-                                    <br />
-                                    <h1 className='text-color-white text-margin-top'>NICHOLAS <br />KLUGE CORRÊA</h1>
-                                    <br />
-                                    <h2 className='text-color-white'>AI Developer</h2>
-                                    <br />                                    
-                                    <p className='text-color-white'>Mestre em Engenharia Elétrica pela PUC-RS. Candidato a Ph.D pela PUC-RS e Rheinische Friedrich-Wilhelms-Universität Bonn (Alemanha).  Pesquisador na área de Machine Learning (ML) com foco em Ética, Fairness, Explainability, e Adversarial ML. Presidente da AI Robotics Ethics Society na PUC-RS.</p>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-6 col-md-12">
-                                <div className='sobre-nos-2-col'>
-                                    <Image width={195} height={195} src={leoImage} alt="Foto do fundador Eduardo Kautz" className='founder-image-padding' />
-                                    <br />
-                                    <h1 className='text-color-white text-margin-top'>LEONARDO <br />ROSA AMADO</h1>
-                                    <br />
-                                    <h2 className='text-color-white'>AI Developer</h2>
-                                    <br />
-                                    <p className='text-color-white'>Doutor em Ciência da Computação pela PUC-RS. Pesquisador de pós-doutorado no centro de ciência de dados da PUCRS. Pesquisador ativo em Machine Learning, Automated Planning e Reinforcement Learning.</p>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </section>
@@ -140,6 +103,43 @@ class OurTeam extends Component {
                                     <h2 className='text-color-white'>Inovação &amp; Gestão</h2>
                                     <br />
                                     <p className='text-color-white'>Doutor em Ciência da Computação pela UFRGS. Diretor de Novos Negócios do Instituto Ceos. Pesquisador em Engenharia de Software e Consultor de Projetos de Ciência, Tecnologia e Inovação.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section className="team-area ptb-100 pb-70 pt-0 bg-black">
+                    <div className="container">
+                        <div className="section-title sobre-nos center-everything">
+                            <br />
+                            <br />
+                            <br />
+                            <h2 className='text-color-white'>Conselho de Tecnologia</h2>
+                        </div>
+                        <div className='flex-about-us'>
+                            <div className="col-lg-6 col-md-12">
+                                <div className='sobre-nos-2-col'>
+                                    <Image width={195} height={195} src={nichoImage} alt="Foto do fundador Eduardo Kautz" className='founder-image-padding' />
+                                    <br />
+                                    <h1 className='text-color-white text-margin-top'>NICHOLAS <br />KLUGE CORRÊA</h1>
+                                    <br />
+                                    <h2 className='text-color-white'>AI Developer</h2>
+                                    <br />                                    
+                                    <p className='text-color-white'>Mestre em Engenharia Elétrica pela PUC-RS. Candidato a Ph.D pela PUC-RS e Rheinische Friedrich-Wilhelms-Universität Bonn (Alemanha).  Pesquisador na área de Machine Learning (ML) com foco em Ética, Fairness, Explainability, e Adversarial ML. Presidente da AI Robotics Ethics Society na PUC-RS.</p>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-6 col-md-12">
+                                <div className='sobre-nos-2-col'>
+                                    <Image width={195} height={195} src={leoImage} alt="Foto do fundador Eduardo Kautz" className='founder-image-padding' />
+                                    <br />
+                                    <h1 className='text-color-white text-margin-top'>LEONARDO <br />ROSA AMADO</h1>
+                                    <br />
+                                    <h2 className='text-color-white'>AI Developer</h2>
+                                    <br />
+                                    <p className='text-color-white'>Doutor em Ciência da Computação pela PUC-RS. Pesquisador de pós-doutorado no centro de ciência de dados da PUCRS. Pesquisador ativo em Machine Learning, Automated Planning e Reinforcement Learning.</p>
                                 </div>
                             </div>
 
